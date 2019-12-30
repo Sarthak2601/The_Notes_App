c83525e6ac311ce0eb311b778d2573cfb1571154
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2019-12-30 23:21:45 +0530

    Initial commit

---
+++ app/src/main/java/com/njack/architecturedemo/NoteAdapter.java
@@ -0,0 +1,61 @@
+package com.njack.architecturedemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item,parent,false);
+
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+
+        Note currentNote = notes.get(position);
+        holder.textView_title.setText(currentNote.getTitle());
+        holder.textView_description.setText(currentNote.getDescription());
+        holder.textView_priority.setText(String.valueOf(currentNote.getPriority()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes){
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder{
+
+        private TextView textView_title;
+        private TextView textView_description;
+        private TextView textView_priority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textView_title = itemView.findViewById(R.id.text_view_title);
+            textView_description = itemView.findViewById(R.id.text_view_description);
+            textView_priority = itemView.findViewById(R.id.text_view_priority);
+        }
+    }
+}
